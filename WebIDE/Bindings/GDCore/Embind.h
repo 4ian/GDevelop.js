d02aac77fbc632c5f014e682bed2384d75462a9f
Florian Rival <Florian.rival@gmail.com>
2014-06-30 21:54:00 +0200

    Imported from GD repository.

---
+++ WebIDE/Bindings/GDCore/Embind.h
@@ -0,0 +1,8 @@
+/*
+ * Game Develop Core
+ * Copyright 2008-2014 Florian Rival (Florian.Rival@gmail.com). All rights reserved.
+ * This project is released under the GNU Lesser General Public License.
+ */
+
+#if defined(EMSCRIPTEN)
+#endif
---
+++ WebIDE/postjs.js
@@ -0,0 +1,2 @@
+Module = Module || {};
+var gd = Module;
